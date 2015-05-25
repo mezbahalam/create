4f009cc178cf63961876be7f58598660f8c1c920
tryhendri <hendri.firmana@gmail.com>
2015-05-25 09:42:36 +0700

    push only for development -- dont merge with master

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :require_http_auth  if Rails.env.production?
 
   def require_http_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == "tone" && password == "musictshirts1"
     end
   end
-
-
 
 end
