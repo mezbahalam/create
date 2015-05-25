25e33174e92cc146b4a4ddeb76f79109e17edf38
tryhendri <hendri.firmana@gmail.com>
2015-05-25 09:56:31 +0700

    Fix http authentication -- dont merge with master

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :require_http_auth  if Rails.env.production?
+  before_filter :authenticate_with_http_basic_authentication if Rails.env.production?
 
-  def require_http_auth
+  private
+
+ 	def authenticate_with_http_basic_authentication
     authenticate_or_request_with_http_basic("Authentication") do |name, password|
       name == 'tone' && password == 'musictshirts1'
     end
   end
-
 end
